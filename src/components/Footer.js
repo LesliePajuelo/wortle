761c76fc19a4e8c32e271572ec04db3664dd6a05
nmfretz <nmfretz@gmail.com>
2022-02-24 22:44:42 -0800

    add attributions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,31 @@
 const Footer = () => {
   return (
-    <div className="is-flex is-justify-content-center mt-3">
-      <p className="has-text-white">
+    <div className="is-flex is-flex-direction-column is-align-items-center is-justify-content-center mt-3">
+      <p className="has-text-white pb-3">
         <span>Created by </span>
         <a className="has-text-white is-underlined" href="https://www.nathanfretz.me/" target="_blank">
           Nathan Fretz
+        </a>
+      </p>
+      <p className="has-text-white is-size-7">
+        <span>Inspired by </span>
+        <a
+          className="has-text-white is-underlined"
+          href="https://www.nytimes.com/games/wordle/index.html"
+          target="_blank"
+        >
+          Wordle
+        </a>
+        <span> (created by </span>
+        <a className="has-text-white is-underlined" href="https://twitter.com/powerlanguish" target="_blank">
+          Josh Wardle
+        </a>
+        <span>)</span>
+      </p>
+      <p className="has-text-white is-size-7">
+        <span>Pokémon data from </span>
+        <a className="has-text-white is-underlined" href="https://pokeapi.co/" target="_blank">
+          PokéApi
         </a>
       </p>
     </div>
