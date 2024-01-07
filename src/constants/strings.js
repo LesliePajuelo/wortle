ed5519a9b09bdf3e29d8f15fb48a7906738c7cf1
EnviroTechie <et@EnviroTchiesAir.verizon.net>
2024-01-06 22:42:31 -0500

    Add endless replays and change type words for pogo style icons)

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,5 +1,5 @@
 export const dailyGameUrl = "https://www.sqwordle.io/";
-export const safariZoneUrl = "https://www.safarizone.sqwordle.io/";
+export const safariZoneUrl = "http://localhost:3000";
 // FOR BETA SITE:
 // export const dailyGameUrl = "https://sqwordle-beta.netlify.app/";
 // export const safariZoneUrl = "https://sqwordle-safari-zone-beta.netlify.app/";
