ca6637442baba0b197728a28e1a210b9f13d5a7a
Leslie pajuelo <l.l.pajuelo@gmail.com>
2024-01-13 12:20:06 -0500

    fix safari zone

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,5 +1,5 @@
-export const dailyGameUrl = "https://www.sqwordle.io/";
-export const safariZoneUrl = "http://localhost:3000";
+export const dailyGameUrl = "https://wortle.envirotechie.com/";
+export const safariZoneUrl = "https://wortle.envirotechie.com/";
 // FOR BETA SITE:
 // export const dailyGameUrl = "https://sqwordle-beta.netlify.app/";
 // export const safariZoneUrl = "https://sqwordle-safari-zone-beta.netlify.app/";
